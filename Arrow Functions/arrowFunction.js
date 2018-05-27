891f61c4134683e0d9ca4b43f755ecf6cf12b4a6
nidhinkumar <nidhinkumar@codingtown.com>
2018-05-27 21:21:24 +0530

    Add arrow functions

---
+++ Arrow Functions/arrowFunction.js
@@ -0,0 +1,17 @@
+//#1
+// function funcName(params) {
+//    return params + 2;
+//    console.log("params is", params);
+//  } //#old method
+
+// var funcName = (params) => params + 2  //# new method no need of function
+
+// funcName(2);
+
+//#2
+// function double(num) {
+//   return num * 2;
+// }
+
+var double = num => num * 2
+double(3);
